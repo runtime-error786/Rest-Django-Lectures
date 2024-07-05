497ea43df709362f8176ed30100601e0b6292882
runtime-error786 <mustafa782a@gmail.com>
2024-07-05 21:47:06 +0500

    cursor pagination

--- prac/api/page.py
+++ prac/api/page.py
@@ -5,8 +5,14 @@
 #     max_page_size=4
 
 
-from rest_framework.pagination import LimitOffsetPagination
+# from rest_framework.pagination import LimitOffsetPagination
 
-class MY_page(LimitOffsetPagination):
-    default_limit = 3
-    max_limit = 5
+# class MY_page(LimitOffsetPagination):
+#     default_limit = 3
+#     max_limit = 5
+
+from rest_framework.pagination import CursorPagination
+
+class MY_page(CursorPagination):
+    page_size = 2
+    ordering = 'email'
